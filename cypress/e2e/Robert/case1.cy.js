01c4a0e74222cdbffdb2d5954a7c61ad02624aac
Robet Zheng <zhengbaook@hotmail.com>
2026-02-09 19:10:59 +0800

    update phone no. to random

--- cypress/e2e/Robert/case1.cy.js
+++ cypress/e2e/Robert/case1.cy.js
@@ -15,8 +15,8 @@ describe('Case 1 - Basic Navigation and Interaction', () => {
 		cy.get(':nth-child(4) > .sc-jrsJWt > [data-testid="TextInput"]').type('John')
 		cy.get(':nth-child(5) > .sc-jrsJWt > [data-testid="TextInput"]').type('Doe')
 		cy.get(':nth-child(6) > .sc-jrsJWt > [data-testid="TextInput"]').type('test@test.com')
-		cy.get('[name="LL012"]').type('852')
-		cy.get('[name="LL011"]').type('88888888')
+		// cy.get('[name="LL012"]').type('852')
+		cy.get('[name="LL011"]').type(`8${Math.floor(Math.random() * 1000000).toString().padStart(6, '0')}8`);
 		cy.get(':nth-child(9) > [data-testid="Dropdown"] > .widthChange > .css-1994fnm-control > .css-1o3m3d8').click({ force: true })
 		cy.get('#react-select-3-option-2').click()
         cy.get(':nth-child(10) > [data-testid="Dropdown"] > .widthChange > .css-1994fnm-control').click({ force: true })
